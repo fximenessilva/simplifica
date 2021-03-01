3470de7fa1bf242da109c45782b6a6bd8ee6937d
francisco ximenes <fximenessilva@gmail.com>
2021-03-01 14:34:22 +0100

    add store

--- omdb/src/App.jsx
+++ omdb/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+
+import { Provider } from 'react-redux';
+import configureStore from './redux/configureStore';
+
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+const store = configureStore();
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <React.StrictMode>
+      <Provider store={store}>
+        <Header />
+        <Footer />
+      </Provider>
+    </React.StrictMode>
   );
 }
 
