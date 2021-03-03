4bb9dee42765de8acbde8e85b6b409bf83f4f19e
francisco ximenes <fximenessilva@gmail.com>
2021-03-03 16:03:47 +0100

    add loading spinner

---
+++ omdb/src/components/MoviesList/PopularMoviesList/PopularMovies.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import BounceLoader from 'react-spinners/BounceLoader';
+import { requestPopularMovies } from '../../../redux/actions/moviesActions';
+
+import PopularMoviesList from './PopularMoviesList';
+import Loading from '../../Loading/Loading';
+
+const PopularMovies = ({ popularMovies, dispatch }) => {
+  useEffect(() => {
+    if (!popularMovies || !popularMovies.length) {
+      dispatch(requestPopularMovies());
+    }
+  }, [popularMovies]);
+
+  return (
+    <>
+      {!popularMovies
+        ? <BounceLoader color="#E50914" size={50} />
+        : <PopularMoviesList popularMovies={popularMovies} />}
+    </>
+
+  );
+};
+
+function mapStateToProps({ moviesReducer }) {
+  return {
+    popularMovies: moviesReducer.popularMovies,
+  };
+}
+
+export default connect(mapStateToProps)(PopularMovies);
--- omdb/src/components/MoviesList/PopularMoviesList/PopularMoviesList.jsx
+++ omdb/src/components/MoviesList/PopularMoviesList/PopularMoviesList.jsx
@@ -1,37 +1,21 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { requestPopularMovies } from '../../../redux/actions/moviesActions';
 
-const PopularMoviesList = ({ popularMovies, dispatch }) => {
-  useEffect(() => {
-    if (!popularMovies || !popularMovies.length) {
-      dispatch(requestPopularMovies());
-    }
-  }, [popularMovies]);
+const PopularMoviesList = ({ popularMovies }) => (
+  <>
 
-  return (
-    <>
-      {popularMovies && popularMovies?.length && (
-        popularMovies.map((movie) => (
-          <li key={movie.imdbID}>
-            <Link to={`/${movie.imdbID}`}>
-              {movie.Title}
-              <img src={movie.Poster} alt="" />
-            </Link>
-          </li>
-        ))
-      )}
-    </>
+    {popularMovies && popularMovies?.length && (
+      popularMovies.map((movie) => (
+        <li key={movie.imdbID}>
+          <Link to={`/${movie.imdbID}`}>
 
-  );
-};
+            <img src={movie.Poster} alt="" />
+          </Link>
+        </li>
+      ))
+    )}
+  </>
+);
 
-function mapStateToProps({ moviesReducer }) {
-  return {
-    popularMovies: moviesReducer.popularMovies,
-  };
-}
-
-export default connect(mapStateToProps)(PopularMoviesList);
+export default PopularMoviesList;
