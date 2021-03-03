3aa34718cac6e6d9fc956fdb962484b2a4792806
francisco ximenes <fximenessilva@gmail.com>
2021-03-03 20:55:45 +0100

    add splited popular movies array

--- omdb/src/components/MoviesList/PopularMoviesList/PopularMoviesList.jsx
+++ omdb/src/components/MoviesList/PopularMoviesList/PopularMoviesList.jsx
@@ -1,36 +1,12 @@
-/* eslint-disable no-unused-expressions */
+/* eslint-disable arrow-body-style */
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-nested-ternary */
 /* eslint-disable react/prop-types */
-
-
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-
 import React from 'react';
-
 import { Link } from 'react-router-dom';
-
-
 import './styles/PopularMoviesList.css';
 
-const PopularMoviesList = ({ popularMovies, dispatch }) => {
-  let poplrMoviesCopy;
-  let dividedArray;
-
-  useEffect(() => {
-    if (!popularMovies || !popularMovies.length) {
-      dispatch(requestPopularMovies());
-    }
-  }, [popularMovies?.length]);
-
-  if (popularMovies) {
-    poplrMoviesCopy = [...popularMovies];
-    dividedArray = new Array(Math.ceil(poplrMoviesCopy.length / 10)).fill().map((_) => poplrMoviesCopy.splice(0, 10));
-    console.log(dividedArray[0]);
-  }
-
+const PopularMoviesList = ({ popularMovies }) => {
   function printPopularMoviesRow(arr, rowNmbr) {
     return (
       <ul className={`poplrMovie-row poplrMovie-row${rowNmbr}--wrapper`}>
@@ -55,17 +31,16 @@ const PopularMoviesList = ({ popularMovies, dispatch }) => {
     <>
       <div className="poplrMovies-wrapper">
 
-        { dividedArray && dividedArray?.length && (printPopularMoviesRow(dividedArray[0], 1)) }
-        { dividedArray && dividedArray?.length && (printPopularMoviesRow(dividedArray[1], 2)) }
-        { dividedArray && dividedArray?.length && (printPopularMoviesRow(dividedArray[2], 3)) }
-        { dividedArray && dividedArray?.length && (printPopularMoviesRow(dividedArray[3], 4)) }
-        { dividedArray && dividedArray?.length && (printPopularMoviesRow(dividedArray[4], 5)) }
+        { popularMovies && popularMovies?.length && (printPopularMoviesRow(popularMovies[0], 1)) }
+        { popularMovies && popularMovies?.length && (printPopularMoviesRow(popularMovies[1], 2)) }
+        { popularMovies && popularMovies?.length && (printPopularMoviesRow(popularMovies[2], 3)) }
+        { popularMovies && popularMovies?.length && (printPopularMoviesRow(popularMovies[3], 4)) }
+        { popularMovies && popularMovies?.length && (printPopularMoviesRow(popularMovies[4], 5)) }
 
       </div>
 
     </>
   );
 };
-
 
 export default PopularMoviesList;
