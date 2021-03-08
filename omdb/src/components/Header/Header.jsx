7a842ea28f27cb4db2e9bbcbfe681c0303ee51c4
francisco ximenes <fximenessilva@gmail.com>
2021-03-08 20:07:28 +0100

    add feature film styles

--- omdb/src/components/Header/Header.jsx
+++ omdb/src/components/Header/Header.jsx
@@ -1,11 +1,24 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/Header.css';
 
 export default function Header(props) {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
-    <div className="header-wrapper">
+    <div className={scrollPosition === 0 ? 'header-wrapper header-top' : 'header-wrapper header-scrolled'}>
       <header>
 
         <img className="header-logo" src="https://fontmeme.com/permalink/210304/e949283e3c4f31f05b1d2622a0c9a082.png" alt="netflix-font" border="0" />
