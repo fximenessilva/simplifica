5224813cb78ff8004755f21297354d767df7f698
francisco ximenes <fximenessilva@gmail.com>
2021-03-09 17:43:35 +0100

    add movieList styles

--- omdb/src/components/MoviesList/PopularMoviesList/PopularMoviesList.jsx
+++ omdb/src/components/MoviesList/PopularMoviesList/PopularMoviesList.jsx
@@ -11,21 +11,31 @@ const PopularMoviesList = ({ popularMovies }) => {
     return (
       <>
         {rowNmbr === 1 ? <p className="pplr-tag">Popular</p> : null}
+        {rowNmbr === 2 ? <p className="classics-tag">Classics</p> : null}
         <ul className={`poplrMovie-row poplrMovie-row${rowNmbr}--wrapper`}>
+
           {arr.map((movie) => (
             <li key={movie?.imdbID}>
               <Link to={`/${movie?.imdbID}`}>
-                <div className="poplrMovie-img-wrapper">
+                <div className={`poplrMovie-img-wrapper poplrMovie${rowNmbr}-img-wrapper`}>
+
                   <img
+                    id="puta"
                     className="poplrMovie-img"
                     src={movie?.Poster}
                     alt=""
                   />
+
+                  <div className={`poplrMovie-detail poplrMovie-detail-row${rowNmbr}`}>
+                    <h4 className="poplrMovie-detail-title">{movie.Title}</h4>
+                    <p className="poplrMovie-detail-description">as</p>
+                  </div>
                 </div>
               </Link>
             </li>
           ))}
         </ul>
+
       </>
     );
   }
